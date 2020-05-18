a6c89a2eee497fa6cfc736ba45202d10e6dfcb50
duyixian <duyixian1234@qq.com>
2020-05-18 20:12:04 +0800

    MOD:add a endpoint to generate QR code

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask,jsonify
+from flask import Flask, request, send_file, jsonify
+from tempfile import mkstemp
+import qrcode
 
 app = Flask(__name__)
+
+
+@app.route('/qr')
+def make_qr():
+    content = request.values.get('content', 'empty content')
+    img = qrcode.make(content)
+    _, path = mkstemp()
+    img.save(open(path, 'wb'), format='png')
+    return send_file(open(path, 'rb'), mimetype='image/png')
+    
 
 @app.route('/')
 def index():
